c8767fd9943f1d1e7d0416f0f91a486d891f7c9d
sandro <devspuig@pm.me>
2022-02-16 13:34:44 +0100

    Add addresses for rinkeby testnet

--- addresses.ts
+++ addresses.ts
@@ -14,7 +14,23 @@ const addresses = [
                 address: '0x0123123123123123123123123123123123123123'
             },
         ]
-    }
+    },
+    {
+        chainId: 4,
+        bridges: [
+            {
+                name: 'anyswap',
+                address: '0x95dD59343a893637BE1c3228060EE6afBf6F0730'
+            }
+        ],
+        swappers: [
+            {
+                name: 'uni',
+                code: 0,
+                address: '0xE592427A0AEce92De3Edee1F18E0157C05861564'
+            },
+        ]
+    },
 ];
 
 export default addresses;
