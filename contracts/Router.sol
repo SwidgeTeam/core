643b47dd8a0c2e9859164ae4f87497b8a13eadde
sandro <devspuig@pm.me>
2022-04-10 21:25:49 +0200

    Add complete process of swap+bridge

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -7,100 +7,103 @@ import "./dexs/IDEX.sol";
 import "./bridge/IBridge.sol";
 
 contract Router {
-    mapping(uint8 => IDEX) private swapProviders;
     mapping(uint8 => IBridge) private bridgeProviders;
-
-    enum dexCode {
-        Uniswap, // 0
-        Sushiswap // 1
-        // ...
-    }
 
     enum bridgeCode {
         Anyswap // 0
-        // ...
     }
 
     /// @param _anyswapRouterAddress Address of the default bridge
-    /// @param _dexAddresses Array of DEX addresses
-    /// @param _dexCodes Array of DEX identifiers
-    constructor(address _anyswapRouterAddress, address[] memory _dexAddresses, dexCode[] memory _dexCodes) {
-        initializeBridge(_anyswapRouterAddress);
-        initializeDEXs(_dexAddresses, _dexCodes);
-    }
-
-    /// Initialize bridge address
-    /// @dev _anyswapRouterAddress Address of AnyswapRouter contract
-    function initializeBridge(address _anyswapRouterAddress) internal {
+    constructor(address _anyswapRouterAddress) {
         bridgeProviders[uint8(bridgeCode.Anyswap)] = IBridge(_anyswapRouterAddress);
     }
 
-    /// Initialize the mapping of DEX's that router has available in its network
-    /// @dev codes and addresses need to be same length, otherwise we fail
-    /// @param _dexAddresses Array of DEX addresses
-    /// @param _dexCodes Array of DEX identifiers
-    function initializeDEXs(address[] memory _dexAddresses, dexCode[] memory _dexCodes) private {
-        require(_dexAddresses.length == _dexCodes.length, "Addresses count cannot mismatch codes count.");
-        require(_dexAddresses.length != 0, "No swap providers informed.");
-        for (uint8 i = 0; i < _dexAddresses.length; i++) {
-            IDEX provider = IDEX(_dexAddresses[i]);
-            swapProviders[uint8(_dexCodes[i])] = provider;
-        }
+    struct SwapData {
+        address tokenIn;
+        address tokenOut;
+        address payable callAddress;
+        bytes callData;
     }
 
     struct BridgeData {
-        uint8 id;
+        address tokenIn;
+        address receiverAddress;
         uint256 toChainId;
         bytes data;
     }
 
-    struct ExchangeData {
-        uint8 id;
-        bytes data;
-    }
+    event CrossInitiated(
+        uint256 amount,
+        uint256 indexed toChainId,
+        address indexed router,
+        address dstTokenIn,
+        address dstTokenOut,
+        address maker,
+        bytes data
+    );
 
     /// Init the process of swidging
     /// @dev This function is executed on the origin chain
-    /// @param _srcToken Address of the token the user wants to swidge
-    /// @param _srcAmount Amount of source tokens that user wants to move
     function initTokensCross(
-        address _srcToken,
-        address _dstToken,
         uint256 _srcAmount,
-        ExchangeData calldata _srcDEXData,
-        ExchangeData calldata _dstDEXData,
+        SwapData calldata _srcSwapData,
+        SwapData calldata _dstSwapData,
         BridgeData calldata _bridgeData
     ) external payable {
         // Take ownership of user's tokens
-        TransferHelper.safeTransferFrom(_srcToken, msg.sender, address(this), _srcAmount);
-
-        // Get initial balance to compute final bought amount
-        uint256 boughtAmount = IERC20(_dstToken).balanceOf(address(this));
+        TransferHelper.safeTransferFrom(
+            _srcSwapData.tokenIn,
+            msg.sender,
+            address(this),
+            _srcAmount
+        );
 
         // Approve to ZeroEx
-        TransferHelper.safeApprove(_srcToken, _zeroExApproval, _srcAmount);
+        TransferHelper.safeApprove(
+            _srcSwapData.tokenIn,
+            _srcSwapData.callAddress,
+            _srcAmount
+        );
 
-        // Execute ZeroEx
-        (bool success,) = _zeroExMaker.call{value : msg.value}(_data);
+        // Execute swap with ZeroEx and compute final `boughtAmount`
+        uint256 boughtAmount = IERC20(_srcSwapData.tokenOut).balanceOf(address(this));
+        (bool success,) = _srcSwapData.callAddress.call{value : msg.value}(_srcSwapData.callData);
         require(success, "SWAP FAILED");
-
-        // Refund any unspent protocol fees to the sender.
-        msg.sender.transfer(address(this).balance);
-
-        // Compute final amount of bought tokens
-        boughtAmount = IERC20(_dstToken).balanceOf(address(this)) - boughtAmount;
-
-        // Transfer to the user
-        TransferHelper.safeTransfer(_dstToken, msg.sender, boughtAmount);
+        boughtAmount = IERC20(_srcSwapData.tokenOut).balanceOf(address(this)) - boughtAmount;
 
         // Load selected bridge provider
-        IBridge bridge = bridgeProviders[_bridgeData.id];
+        IBridge bridge = bridgeProviders[uint8(bridgeCode.Anyswap)];
 
         // Approve tokens for the bridge to take
-        TransferHelper.safeApprove(_srcCrossToken, address(bridge), amountOut);
+        TransferHelper.safeApprove(
+            _bridgeData.tokenIn,
+            address(bridge),
+            boughtAmount
+        );
 
         // Execute bridge process
-        bridge.send(_srcToken, address(this), amountOut, _bridgeData.toChainId, _bridgeData.data);
+        bridge.send(
+            _bridgeData.tokenIn,
+            address(this),
+            _bridgeData.receiverAddress,
+            boughtAmount,
+            _bridgeData.toChainId,
+            _bridgeData.data
+        );
+
+        // Refund any unspent protocol fees to the sender.
+        payable(msg.sender).transfer(address(this).balance);
+
+        // Emit event for relayer
+        emit CrossInitiated(
+            boughtAmount,
+            _bridgeData.toChainId,
+            _bridgeData.receiverAddress,
+            _dstSwapData.tokenIn,
+            _dstSwapData.tokenOut,
+            _dstSwapData.callAddress,
+            _dstSwapData.callData
+        );
     }
 
     /// Finalize the process of swidging
