11fb5341fa278ad15b228897f5e86f95dbfe1b9f
sandro <devspuig@pm.me>
2022-05-08 16:05:54 +0200

    Add script to update the router's address on all deployed providers

---
+++ scripts/update-router-address.py
@@ -0,0 +1,17 @@
+from brownie import Router, ZeroEx, Anyswap
+
+from scripts.src.deploy import from_deployer
+from scripts.src.addresses import save_addresses, load_addresses
+
+"""
+Updates the Router address on all deployed providers
+"""
+def main():
+    network = 'hardhat'
+    address = load_addresses(network)
+
+    zeroEx = ZeroEx.at(address['swapImpl']['zeroex']['address'])
+    multichain = Anyswap.at(address['bridgeImpl']['anyswap']['address'])
+
+    zeroEx.updateRouter(address['router'], from_deployer)
+    multichain.updateRouter(address['router'], from_deployer)
