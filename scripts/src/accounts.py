5ad94b0bfa00de5a30d45fa04b7265f837c45113
sandro <devspuig@pm.me>
2022-05-08 20:12:34 +0200

    Set config for brownie to use mnemonic from .env for accounts

--- scripts/src/accounts.py
+++ scripts/src/accounts.py
@@ -1,9 +1,13 @@
-from brownie import accounts
+from brownie import accounts, config
 
 """
 Names the accounts with names for the rest of the scripts to use
 """
+mnemonic = config['wallet']['mnemonic']
+acc = accounts.from_mnemonic(mnemonic, 10)
 
-deployer = accounts[0]
-user = accounts[1]
-random = accounts[2]
+old_deployer = acc[0]
+admin = acc[1]
+user = acc[2]
+deployer = acc[3]
+random = acc[4]
