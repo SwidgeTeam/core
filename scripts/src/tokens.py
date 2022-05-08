0ad513c0082722ed992886ae3b12eeb51dd8b466
sandro <devspuig@pm.me>
2022-05-08 16:24:55 +0200

    Change token list to match networks ids

--- scripts/src/tokens.py
+++ scripts/src/tokens.py
@@ -5,9 +5,10 @@ import yaml
 Loads the list of tokens data of a specific domain
 """
 def load_tokens(network):
+    main_network = network.replace('-fork', '')
     with open('tokens.yaml', 'r') as file:
         addresses = yaml.safe_load(file)
-        return addresses[network]
+        return addresses[main_network]
 
 """
 Transfers an `amount` of tokens from `holder_address` to `to_address`
