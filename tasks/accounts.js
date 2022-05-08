dfce20b64320ed8226944e30fa2ff494e4f690e0
sandro <devspuig@pm.me>
2022-05-08 20:26:47 +0200

    Add script to verify Router contract

--- tasks/accounts.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (taskArguments, hre, runSuper) {
-    const accounts = await hre.ethers.getSigners();
-
-    for (const account of accounts) {
-        console.log(account.address);
-    }
-}
--- tasks/index.js
+++
@@ -1,7 +0,0 @@
-const {task} = require("hardhat/config")
-
-task("accounts", "Prints the list of accounts")
-    .setAction(require("./accounts"));
-
-task("verify-router", "Verifies the router contract")
-    .setAction(require('./verify-router'));
