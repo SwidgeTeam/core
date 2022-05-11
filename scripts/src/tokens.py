b998120c9c52aed4982c97e5adf82a175ac40b6e
sandro <devspuig@pm.me>
2022-05-11 16:46:20 +0200

    Only load from explorer when not in memory

--- scripts/src/tokens.py
+++ scripts/src/tokens.py
@@ -14,12 +14,18 @@ def load_tokens(network):
 Transfers an `amount` of tokens from `from_address` to `to_address`
 """
 def transfer_tokens_to(token_address, from_address, to_address, amount):
-    token = Contract(token_address)
+    try:
+        token = Contract(token_address)
+    except:
+        token = Contract.from_explorer(token_address)
     token.transfer(to_address, amount, {'from': from_address})
 
 """
 Approves an `amount` of tokens from `from_address` to `to_address`
 """
 def approve_tokens_to(token_address, from_address, to_address, amount):
-    token = Contract.from_explorer(token_address)
+    try:
+        token = Contract(token_address)
+    except:
+        token = Contract.from_explorer(token_address)
     token.approve(to_address, amount, {'from': from_address})
